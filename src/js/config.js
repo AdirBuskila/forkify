3706fd5dabc78bcf42176501e31bddb4e791e31a
Adir Buskila <Adirbu98@gmail.com>
2022-05-10 10:37:07 +0300

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'ca29fe14-9eb7-4299-be57-3d94aef86890';
+export const MODAL_CLOSE_SEC = 2.5;
